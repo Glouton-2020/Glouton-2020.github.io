08eeee6a326edfa149cbef488ca5893e2960b6d2
Benjamin Glouton <benjie@benjamins-mbp.home>
2020-04-01 18:55:03 -0400

    Invader, bullet, player logic complete

---
+++ Space Invaders/app.js/PlayerBullet.js
@@ -0,0 +1,10 @@
+
+class PlayerBullet extends Bullet {
+    constructor(x, y) {
+        super(x, y);
+    }
+
+    update() {
+        this.y -= 6;
+    }
+}
---
+++ Space Invaders/app.js/alienBullet.js
@@ -0,0 +1,10 @@
+
+class AlienBullet extends Bullet {
+    constructor(x , y) {
+        super(x, y);
+    }
+
+    update() {
+        this.y += 2;
+    }
+}
