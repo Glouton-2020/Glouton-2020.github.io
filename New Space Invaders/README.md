1e630f590045858d828d805ddee7bb7c4ccc2aab
Benjamin Glouton <benjie@benjamins-mbp.home>
2020-04-02 05:32:10 -0400

    Started ReadMe and adding objects to the new JS. Next step is wrting Player Object

--- New Space Invaders/README.md
+++ New Space Invaders/README.md
@@ -1,6 +1,12 @@
-Project Title
+Project Title: Space Invaders
 
 One Paragraph of project description goes here
+
+Enemies will come down from the top of the screen in a grid and move left and right until they hit either edge at which point they will move down and move in the opposite direction away from the edge of the screen. The entire grid moves as a unit, so if one enemy hits an edge, they all move down and move in the other direction. The enemies on the bottom of each column occasionally shoot a projectile down towards the player. Each enemy has a rank which has it own graphic and points awarded when killed. The enemies closest to the player a low rank and the ones on top have a high rank. 
+
+The player will be at the bottom of the screen and limited to moving left and right. They can shoot a projectile upwards, being limited to one shot on the screen at a time. If a player shot hits an enemy, the enemy is removed from the grid with a little explosion and the player awarded some points. All the remaining enemies speed up a little bit. When all the enemies have been killed, a new grid of enemies is created, now faster and more likely to shoot, and the player is awarded another ship. 
+
+When an enemy shot hits the player, the ship is destroyed. If there are remaining ships available, play continues with a new ship. Once all the extra ships have been destroyed, the game ends and the top 10 high scores are displayed. The high scores will be saved in the browser so the player can come back and beat their own high scores from previous sessions. 
 
 Getting Started
 
---
+++ New Space Invaders/app.js
@@ -0,0 +1,146 @@
+
+//
+// Vector2d Object
+///// Vectors will be used for the movement and position of game entities.
+
+var Vector2d = function (x, y) {
+    this.x = x;
+    this.y = y;
+};
+
+// adds vectors v1 & v2 together
+function vectorAdd(v1, v2) {
+    return new Vector2d(v1.x + v2.x, v1.y + v2.y);
+}
+
+// subtract vector v2 from v1
+function vectorSubtract(v1, v2) {
+    return new Vector2d(v1.x - v2.x, v1.y - v2.y);
+}
+
+// multiplies the components of v1 by the scalar number s
+function vectorScalarMultiply(v1, s) {
+    return new Vector2d(v1.x * s, v1.y * s);
+}
+
+// returns the length of vector v
+function vectorLength(v) {
+    return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+// returns a normalized vector v (a vector with length == 1)
+function vectorNormalize(v) {
+    var reciprocal = 1.0 / (vectorLength(v) + 1.0e-037); // Prevent division by zero
+    return vectorScalarMultiply(v, reciprocal);
+}
+
+// Rectangle
+// Rectangles will be used to check for collisions between entities as well as defining the boundary of the game area. 
+// It will also keep track of the size of the entire group of enemies to make them move as a group. 
+
+//Rectangle Object
+////////////////////
+function Rectangle (x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this. width = width;
+    this. height = height;
+}
+// Returns the left edge
+Rectangle.prototype.left = function () {
+    return this.x;
+};
+// Returns the right edge
+Rectangle.prototype.right = function () {
+    return this.x + this.width;
+};
+// Returns the top edge 
+Rectangle.prototype.top = function () {
+    return this.y;
+};
+// Returns the bottom edge
+Rectangle.prototype.bottom = function () {
+    return this.y + this.height;
+};
+// Returns true if the rectangle r2 intersects with this rectangle
+Rectangle.prototype.intersects = function (r2) {
+    return this.right() >= r2.left() && this.left() <= r2.right() &&
+        this.top() <= r2.bottom() && this.bottom() >= r2.top();
+};
+// Returns a rectangle that contains both rectangles r1 and r2
+function rectUnion (r1, r2) {
+    var x, y, width, height;
+
+    if (r1 === undefined) {
+        return r2;
+    }
+    if (r2 === undefined) {
+        return r1;
+    }
+
+    x = Math.min (r1.x, r2.x);
+    y = Math.min(r1.y, r2.y);
+    width = Math.max(r1.right(), r2.right() ) - Math.min(r1.left(), r2.left()
+    );
+    height = Math.max(r1.bottom(), r2.bottom() ) - Math.min(r1.top(), r2.top()
+    );
+
+    return new Rectangle(x, y, width, height);
+}
+
+// Random Number Function
+////////////////////////////
+//Returns an integer in the range 0 - max
+function randomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+//Entity root class
+/////////
+// Entity Object
+///////////////////
+
+function Entity(position, speed, direction) {
+    this.position = position; // position of the center of the entity
+    this.speed = speed; //speed of motion
+    this.direction = direction; //direction of motion 
+    this.time = 0;  // system time in seconds
+    this.width = 5; //width of the entity
+    this.height = 5; //height of the entity
+    this.hp = 1; // hit points
+}
+// dt (delta time) is the time difference since the last frame
+Entity.prototype.update = function (dt) {
+    this.time += dt;
+};
+//collision rectangle centered at position
+Entity.prototype.collisionRect = function () {
+    return new Rectangle (this.position.x - this.width/2, 
+                            this.position.y - this.height/2,
+                            this.width, 
+                            this.height);
+};
+
+//Enemy Object
+///////////////
+
+function Enemy(position, speed, direction, rank) {
+    Entity.call(this, position, speed, direction);
+
+    this.width = 13;
+    this.height = 10;
+    this.rank = rank;
+}
+Enemy.prototype = Object.create(Entity.prototype);
+
+Enemy.prototype.update = function (dt) {
+    Entity.prototype.update.call(this, dt);
+    if (this.collisionRect().top() <= 0 ||
+        this.collisionRect().bottom() >=
+    game.gameFieldrect().bottom() ) {
+        this.direction.y *= -1;
+    }
+};
+
+//Player Object
+/////////////////
