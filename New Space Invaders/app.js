29a27fb4e5b9e9dce0cc3073a1fdbf07351c10db
Benjamin Glouton <benjie@benjamins-mbp.home>
2020-04-02 12:31:33 -0400

    created player

--- New Space Invaders/app.js
+++ New Space Invaders/app.js
@@ -144,3 +144,21 @@ Enemy.prototype.update = function (dt) {
 
 //Player Object
 /////////////////
+
+function Player(position, speed, direction) {
+    Entity.call(this, position, speed, direction);
+
+    this.width = 20;
+    this.height = 10;
+}
+
+Player.prototype = Object.create(Entity.prototype);
+
+Player.prototype.update = function (dt) {
+    Entity.prototype.update.call(this, dt);
+    if (this.collisionRect().top() <= 0 ||
+        this.collisionRect().bottom() >= 
+    game.gameFieldRect().bottom() ) {
+        this.direction.y *= -1;
+    }
+};
